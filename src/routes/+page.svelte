03c727586249e037ca732e1047070b183ab6a2e8
Kuldeep Singh <kuldeep_s@nid.edu>
2025-08-01 14:05:20 +0530

    0.1

---
+++ src/routes/+page.svelte
@@ -0,0 +1,59 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<body>
+        <div class="titleArt">
+			<div class="titlewords">
+				<h1 id="typedtext" class="title"><span>When</span> <span>Women</span> <span>Make</span> <span>Headlines</span>
+				</h1>
+			</div>
+			<div class="titleCredits">
+				<h2 class="subTitleArt">A visual essay about the (mis)representation of women in the news</h2>
+				<p class="credits">By <a href="https://pudding.cool/author/leonardo-nicoletti" class="creditLink">​​Leonardo
+						Nicoletti</a>
+					and <a href="https://pudding.cool/author/sahiti-sarva" class="creditLink">Sahiti Sarva</a></p>
+			</div>
+            <section class="intro-section">
+			<p class="description"><span class="padded-dropcap">T</span>oday, research has suggested that women are
+				significantly less likely to make the news compared to men. In the <a href="https://whomakesthenews.org/wp-content/uploads/2021/07/GMMP2020.ENG_.FINAL20210713.pdf">most recent
+					report</a> published by the <a href="https://waccglobal.org/our-work/global-media-monitoring-project-gmmp">Global Media Monitoring Project
+				(GMMP)</a>, the largest and longest running research on gender in the world’s news media, women were found to
+				make up just 24% of news subjects and sources reported. According to this report, this number has not changed
+				since 2010.</p>
+			<p class="description">In the context of news, headlines <a href="https://thereader.mitpress.mit.edu/headlines-journalism"> introduce, frame and contextualize</a> a news
+				story. Furthermore, research within the fields of educational and experimental psychology has demonstrated that
+				news headlines can have a <a href="https://www.newyorker.com/science/maria-konnikova/headlines-change-way-think">disproportionate impact on
+					the reader’s mind</a>, and that misleading headlines <a href="https://psycnet.apa.org/record/2014-44652-001">can
+					bias readers toward a specific interpretation</a>.</p>
+			<p class="description">So, if women are underrepresented in the news to begin with, what does it look like when
+				women <i>do</i> make headlines? And how have headlines about women changed over time?</p>
+			<p class="description">To explore these questions, we have visualized the language used in women-centered
+				headlines and how this language has (or has not) changed over time. Using keywords associated with the word
+				“woman” (like girl, mother and lady), we collected and analyzed 382,139 headlines published between 2005 and
+				2021 by the top English-language news publications and news agencies in four countries: The United States of
+				America (US), India, South Africa, and the United Kingdom (UK). A total of 186 publications were considered
+				(i.e. 24 publications in South Africa, 51 publications in India, 57 publications in the UK, and 54 publications
+				in the US).</p>
+		</section>
+  <div id="observablehq-viewof-svg-3e856917"></div>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@observablehq/inspector@5/dist/inspector.css">
+  <script type="module">
+  import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
+  import define from "https://api.observablehq.com/d/30a84ebdc808c7ff.js?v=4";
+  new Runtime().module(define, name => {
+  if (name === "viewof svg") return new Inspector(document.querySelector("#observablehq-viewof-svg-3e856917"));
+  });
+  </script>
+
+  <div id="observablehq-x-dccc87f4"></div>
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@observablehq/inspector@5/dist/inspector.css">
+<script type="module">
+import {Runtime, Inspector} from "https://cdn.jsdelivr.net/npm/@observablehq/runtime@5/dist/runtime.js";
+import define from "https://api.observablehq.com/d/30a84ebdc808c7ff@783.js?v=4";
+new Runtime().module(define, name => {
+  if (name === "x") return new Inspector(document.querySelector("#observablehq-x-dccc87f4"));
+});
+</script>
+
+		</div>
+</body>
